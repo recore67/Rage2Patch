57cc85eedb128c6e9466531ecdcba7962669c53c
salman ameeri <29689492+recore67@users.noreply.github.com>
2025-06-24 19:13:26 +0300

    Upload

--- CChromaEditorLibrary/ChromaThread.cpp
+++ CChromaEditorLibrary/ChromaThread.cpp
@@ -6,12 +6,65 @@ using namespace ChromaSDK;
 using namespace std;
 using namespace std::chrono;
 
+uintptr_t ReadPointerOffset(HANDLE procHandle, uintptr_t addy, int offset) {
+	BYTE array[8];
+	SIZE_T bytesRead;
+	ReadProcessMemory(procHandle, (LPCVOID)(addy + offset), array, sizeof(array), &bytesRead);
+	return *reinterpret_cast<uintptr_t*>(array);
+}
+
+uintptr_t ReadPointer(HANDLE procHandle, uintptr_t addy, const std::vector<int>& offsets) {
+	uintptr_t intPtr = addy;
+	for (auto offset : offsets) {
+		intPtr = ReadPointerOffset(procHandle, intPtr, offset);
+	}
+	return intPtr;
+}
+
+std::vector<BYTE> ReadBytes(HANDLE procHandle, uintptr_t addy, size_t bytes) {
+	std::vector<BYTE> array(bytes);
+	SIZE_T bytesRead;
+	ReadProcessMemory(procHandle, (LPCVOID)addy, array.data(), bytes, &bytesRead);
+	return array;
+}
+
+BYTE ReadValue(HANDLE procHandle, const std::vector<int>& offsets, int valueOffset, uintptr_t moduleBase) {
+	uintptr_t pointerAddress = ReadPointer(procHandle, moduleBase, offsets) + valueOffset;
+	return ReadBytes(procHandle, pointerAddress, 1)[0];
+}
+
+bool WriteBytes(HANDLE procHandle, uintptr_t addy, int value) {
+	SIZE_T bytesWritten;
+	return WriteProcessMemory(procHandle, (LPVOID)addy, &value, 4, &bytesWritten);
+}
+
+bool WriteValue(HANDLE procHandle, const std::vector<int>& offsets, int valueOffset, uintptr_t moduleBase, int value) {
+	uintptr_t pointerAddress = ReadPointer(procHandle, moduleBase, offsets) + valueOffset;
+	return WriteBytes(procHandle, pointerAddress, value) != 0;
+}
+
+void updateSensitivity(HANDLE handle, int& initialSens, const std::vector<int> offsets, long baseOffset, uintptr_t baseAddress, bool condition) {
+	int currentValue = ReadValue(handle, offsets, baseOffset, baseAddress);
+
+	if (initialSens == NULL) {
+		if (currentValue > 0) {
+			initialSens = currentValue;
+		}
+	}
+	else {
+		if (!condition && currentValue != initialSens) {
+			initialSens = currentValue;
+		}
+	}
+}
+
 ChromaThread* ChromaThread::_sInstance = new ChromaThread();
 
 ChromaThread::ChromaThread()
 {
 	_mThread = nullptr;
 	_mWaitForExit = true;
+	Start();
 }
 
 ChromaThread* ChromaThread::Instance()
@@ -56,30 +109,76 @@ void ChromaThread::ProcessAnimations(float deltaTime)
 void ChromaThread::ChromaWorker()
 {
 	// get current time
-	high_resolution_clock::time_point timer = high_resolution_clock::now();
-	high_resolution_clock::time_point timerLast = high_resolution_clock::now();
+	//high_resolution_clock::time_point timer = high_resolution_clock::now();
+	//high_resolution_clock::time_point timerLast = high_resolution_clock::now();
 
-	_mWaitForExit = true;
+	//_mWaitForExit = true;
 
-	while (_mWaitForExit)
-	{
-		// get current time
-		timer = high_resolution_clock::now();
+	HWND hwnd = FindWindowA(NULL, "RAGE2");
 
-		// get time in seconds
-		duration<double, milli> time_span = timer - timerLast;
-		float deltaTime = (float)(time_span.count() / 1000.0f);
-		timerLast = timer;
+	uintptr_t baseAddress = (uintptr_t)GetModuleHandle(L"RAGE2.exe");
+	long isInVehicleBaseOffset = 0x30;
+	const std::vector<int> isInVehicleOffsets = { 0x02D705F0, 0x48, 0x8, 0x28, 0xC08, 0x408, 0x8 };
 
-		ProcessAnimations(deltaTime);
+	long SensitivtyBaseOffset = 0x4C8;
+	const std::vector<int> SensitivtyOffsets = { 0x02D7D228, 0xD8, 0x18, 0x1C0, 0x410, 0x0 };
 
-		if (!_mWaitForExit)
-		{
-			break;
+	long VSensitivtyBaseOffset = 0x4CC;
+	const std::vector<int> VSensitivtyOffsets = { 0x02D7DC28, 0x160, 0x5E0, 0x288, 0x18, 0x1C0, 0x378 };
+
+	int initialVSens = NULL;
+	int initialXSens = NULL;
+
+	bool revertSens = false;
+
+	if (hwnd != NULL) {
+		DWORD procID;
+		GetWindowThreadProcessId(hwnd, &procID);
+		HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
+
+		while (procID != NULL) {
+			//Get and store initial Sensitivity, let player change sens when on foot
+			updateSensitivity(handle, initialVSens, VSensitivtyOffsets, VSensitivtyBaseOffset, baseAddress, revertSens);
+			updateSensitivity(handle, initialXSens, SensitivtyOffsets, SensitivtyBaseOffset, baseAddress, revertSens);
+
+			auto isInVehicle = ReadValue(handle, isInVehicleOffsets, isInVehicleBaseOffset, baseAddress);
+			if (isInVehicle == 1) {
+				if (!revertSens) {
+					WriteValue(handle, VSensitivtyOffsets, VSensitivtyBaseOffset, baseAddress, 300);
+					WriteValue(handle, SensitivtyOffsets, SensitivtyBaseOffset, baseAddress, 100);
+				}
+				revertSens = true;
+			}
+			else {
+				if (revertSens) {
+					WriteValue(handle, VSensitivtyOffsets, VSensitivtyBaseOffset, baseAddress, initialVSens);
+					WriteValue(handle, SensitivtyOffsets, SensitivtyBaseOffset, baseAddress, initialXSens);
+					revertSens = false;
+				}
+			}
 		}
-			
-		this_thread::sleep_for(chrono::milliseconds(1));
 	}
+
+
+	//while (_mWaitForExit)
+	//{
+	//	// get current time
+	//	//timer = high_resolution_clock::now();
+
+	//	// get time in seconds
+	//	//duration<double, milli> time_span = timer - timerLast;
+	//	//float deltaTime = (float)(time_span.count() / 1000.0f);
+	//	//timerLast = timer;
+
+	//	//ProcessAnimations(deltaTime);
+
+	//	if (!_mWaitForExit)
+	//	{
+	//		break;
+	//	}
+
+	//	this_thread::sleep_for(chrono::milliseconds(1));
+	//}
 
 	_mThread = nullptr;
 }
